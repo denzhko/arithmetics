1cb935231669258e41a714e46540116f0262cac5
denzhko <61680009+denzhko@users.noreply.github.com>
2025-11-24 22:46:32 +0300

    Documentation added for package and Add func

--- calcs.go
+++ calcs.go
@@ -1,5 +1,13 @@
+// Package arithmetics provides basic arithmetic operations.
+// It is designed to be simple, clear, and easy to use in Go projects.
+
 package arithmetics
 
+// Add returns the sum of two integers.
+//
+// An addend is a number that is added to another number.
+// The sum is the result of adding the two addends together.
+// For more information on addition, see: https://www.mathsisfun.com/numbers/addition.html
 func Add(left, right int) int {
 	return left + right
 }
